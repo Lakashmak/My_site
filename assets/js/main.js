05b805412f70bf9f7a89e6ede5d4eae7b48c63d0
Lakashmak <lakashmak@gmail.com>
2026-05-05 02:03:39 +0500

    v1.0.5: nav is prepared for expansion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,20 @@
-fetch('components/header.html').then(r => r.text()).then(html => {
-    document.getElementById('header').innerHTML = html;
+Promise.all([
+    fetch('components/header.html').then(r => r.text()),
+    fetch('components/footer.html').then(r => r.text()),
+]).then(([headerHtml, footerHtml]) => {
+    document.getElementById('header').innerHTML = headerHtml;
+    document.getElementById('footer').innerHTML = footerHtml;
+    
+    applyStyles();
 });
 
-fetch('components/footer.html').then(r => r.text()).then(html => {
-    document.getElementById('footer').innerHTML = html;
-});
-
-let height = window.innerHeight;
-
-const elements = document.querySelectorAll('.bg');
-
-elements.forEach(el => {
-    el.style.minHeight = height + 'px';
-});
+function applyStyles() {
+	const bg = document.querySelectorAll('.bg');
+	const nav = document.querySelectorAll('nav');
+	
+	let height = window.innerHeight;
+	let width = window.innerWidth;
+	
+	bg.forEach(el => { el.style.minHeight = height + 'px'; });
+	nav.forEach(el => { el.style.width = width + 'px'; });
+}
