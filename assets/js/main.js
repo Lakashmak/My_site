7795a84e838fe189c94a574f6563aa6f160852aa
Lakashmak <lakashmak@gmail.com>
2026-05-04 20:28:18 +0500

    v1.0.2: fixed a CSS bug with height via JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,11 @@ fetch('components/header.html').then(r => r.text()).then(html => {
 fetch('components/footer.html').then(r => r.text()).then(html => {
     document.getElementById('footer').innerHTML = html;
 });
+
+let height = window.innerHeight;
+
+const elements = document.querySelectorAll('.bg');
+
+elements.forEach(el => {
+    el.style.minHeight = height + 'px';
+});
